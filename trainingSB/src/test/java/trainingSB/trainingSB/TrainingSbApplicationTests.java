0fa75f2ca835af3736db389020ed8ba10eae2a25
Mahendra545 <mahendrasanebunny@gmail.com>
2022-07-02 19:47:31 +0530

    adding project

---
+++ trainingSB/src/test/java/trainingSB/trainingSB/TrainingSbApplicationTests.java
@@ -0,0 +1,13 @@
+package trainingSB.trainingSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

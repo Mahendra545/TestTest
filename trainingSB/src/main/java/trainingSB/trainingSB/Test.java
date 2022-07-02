ec4b2823de6e71332aabbb76312363f56d50141f
Mahendra545 <mahendrasanebunny@gmail.com>
2022-07-02 19:55:10 +0530

    Adding param

--- trainingSB/src/main/java/trainingSB/trainingSB/Test.java
+++ trainingSB/src/main/java/trainingSB/trainingSB/Test.java
@@ -1,14 +1,15 @@
 package trainingSB.trainingSB;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
 
-	@GetMapping("/test")
-	public String get() {
-		return "welcome";
+	@GetMapping("/test/{name}")
+	public String get(@PathVariable("name") String name) {
+		return "welcome "+name;
 	}
 	
 }
